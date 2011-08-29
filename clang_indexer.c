b1626aaa36893734359320fa1a5c91f0e1ab4152
Kevin Wu Won <exclipy@gmail.com>
2011-08-30 09:27:15 +1000

    Print all references and filter to those with a known file

--- clang_indexer.c
+++ clang_indexer.c
@@ -5,28 +5,30 @@ enum CXChildVisitResult visitor(
         CXCursor parent,
         CXClientData client_data)
 {
-    CXFile file;
-    unsigned int line, column, offset;
-    clang_getInstantiationLocation(
-            clang_getCursorLocation(cursor),
-            &file, &line, &column, &offset);
-
     CXCursor refCursor = clang_getCursorReferenced(cursor);
-    CXFile refFile;
-    unsigned int refLine, refColumn, refOffset;
-    clang_getInstantiationLocation(
-            clang_getCursorLocation(refCursor),
-            &refFile, &refLine, &refColumn, &refOffset);
+    if (!clang_equalCursors(refCursor, clang_getNullCursor())) {
+        CXFile file;
+        unsigned int line, column, offset;
+        clang_getInstantiationLocation(
+                clang_getCursorLocation(cursor),
+                &file, &line, &column, &offset);
 
-    if (clang_getCursorKind(cursor) == CXCursor_TypeRef) {
-        printf("'%s' at %s:%d:%d refers to %s at %s:%d:%d\n",
-                clang_getCString(clang_getCursorSpelling(cursor)),
-                clang_getCString(clang_getFileName(file)),
-                line, column,
-                clang_getCString(clang_getCursorUSR(refCursor)),
-                clang_getCString(clang_getFileName(refFile)),
-                refLine, refColumn
-                );
+        CXFile refFile;
+        unsigned int refLine, refColumn, refOffset;
+        clang_getInstantiationLocation(
+                clang_getCursorLocation(refCursor),
+                &refFile, &refLine, &refColumn, &refOffset);
+
+        if (clang_getFileName(file).data && clang_getFileName(refFile).data) {
+            printf("'%s' at %s:%d:%d refers to %s at %s:%d:%d\n",
+                    clang_getCString(clang_getCursorSpelling(cursor)),
+                    clang_getCString(clang_getFileName(file)),
+                    line, column,
+                    clang_getCString(clang_getCursorUSR(refCursor)),
+                    clang_getCString(clang_getFileName(refFile)),
+                    refLine, refColumn
+                    );
+        }
     }
     return CXChildVisit_Recurse;
 }
@@ -39,6 +41,7 @@ int main(int argc, const char* argv[]) {
         0, 0,
         CXTranslationUnit_None);
 
+    // Print any errors or warnings
     int n = clang_getNumDiagnostics(tu);
     if (n > 0) {
         int nErrors = 0;
@@ -48,26 +51,18 @@ int main(int argc, const char* argv[]) {
                 diag,
                 clang_defaultDiagnosticDisplayOptions());
             fprintf(stderr, "%s\n", clang_getCString(string));
-            clang_disposeString(string);
             if (clang_getDiagnosticSeverity(diag) == CXDiagnostic_Error
                     || clang_getDiagnosticSeverity(diag) == CXDiagnostic_Fatal)
                 nErrors++;
         }
-        if (nErrors) {
-            clang_disposeTranslationUnit(tu);
-            clang_disposeIndex(index);
+        if (nErrors) // quit if we found any errors
             return 1;
-        }
     }
 
-    ;
     clang_visitChildren(
             clang_getTranslationUnitCursor(tu),
             &visitor,
             0);
 
-
-    clang_disposeTranslationUnit(tu);
-    clang_disposeIndex(index);
     return 0;
 }
