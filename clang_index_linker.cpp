792fcb0b8f7619069a154af6a671f92e14db17c1
Kevin Wu Won <exclipy@gmail.com>
2011-09-01 23:50:49 +1000

    The linker merges together the files given as arguments

--- clang_index_linker.cpp
+++ clang_index_linker.cpp
@@ -1,11 +1,24 @@
 #include "clang_index_parser.hpp"
 #include "clang_index_printer.hpp"
 
+#include <fstream>
 #include <iostream>
 #include <boost/foreach.hpp>
 
-int main() {
-    Index parsedResult = parseIndex(std::cin);
+int main(int argc, char* argv[]) {
+    Index mergedIndex;
 
-    printIndex(std::cout, parsedResult);
+    for (int i = 1; i < argc; i++) {
+        std::ifstream file(argv[i]);
+        if (file.fail()) {
+            std::cerr << argv[i] << " cannot be opened" << std::endl;
+        } else {
+            Index index = parseIndex(file);
+            BOOST_FOREACH(const Index::value_type& it, index) {
+                mergedIndex[it.first].insert(it.second.begin(), it.second.end());
+            }
+        }
+    }
+
+    printIndex(std::cout, mergedIndex);
 }
