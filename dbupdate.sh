713b47acdc2f7ac1e3ae54c7db6b60b524cae0a0
Kevin Wu Won <exclipy@gmail.com>
2011-09-18 14:13:36 +1000

    Fix stupid bugs with dbupdate.sh

--- dbupdate.sh
+++ dbupdate.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+add_to_index() {
+    echo dbadd index.db `cat .clang_complete` $1
+    dbadd index.db `cat .clang_complete` $1
+}
+
+remove_from_index() {
+    echo dbrm index.db $1.i.gz
+    dbrm index.db $1.i.gz
+}
+
 find . -name "*.cpp" | sort > files2.txt
 
 if [ ! -f index.db -o ! -f files.txt ]
@@ -8,8 +18,7 @@ then
     dbclear index.db
     for i in `cat files2.txt`
     do
-        echo dbadd `cat .clang_complete` indexdb $i
-        dbadd indexdb `cat .clang_complete` $i
+        add_to_index $i
     done
     mv files2.txt files.txt
     exit
@@ -21,23 +30,19 @@ comm -23 files2.txt files.txt > filesadded.txt
 
 for i in `cat filesadded.txt`
 do
-    echo dbadd `cat .clang_complete` indexdb $i
-    dbadd indexdb `cat .clang_complete` $i
+    add_to_index $i
 done
 
 for i in `comm -23 files.txt files2.txt` # removed files
 do
-    echo dbrm index.db $i.i.gz
-    dbrm index.db $i.i.gz
+    remove_from_index $i
 done
 
 # modified files (that weren't added)
 for i in `comm -23 <(find . -newer files.txt -name "*.cpp"| sort) filesadded.txt`
 do
-    echo dbrm index.db $i.i.gz
-    dbrm index.db $i.i.gz
-    echo dbadd `cat .clang_complete` indexdb $i
-    dbadd indexdb `cat .clang_complete` $i
+    remove_from_index $i
+    add_to_index $i
 done
 
 rm filesadded.txt
