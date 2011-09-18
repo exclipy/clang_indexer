0806d653b312fcdc7f237be8b5dd9d6f40ef88e8
Kevin Wu Won <exclipy@gmail.com>
2011-09-18 14:21:03 +1000

    Removed files should also have their .i.gz files removed

--- dbupdate.sh
+++ dbupdate.sh
@@ -8,12 +8,13 @@ add_to_index() {
 remove_from_index() {
     echo dbrm index.db $1.i.gz
     dbrm index.db $1.i.gz
+    echo rm $1.i.gz
+    rm $1.i.gz
 }
 
 find . -name "*.cpp" | sort > files2.txt
 
-if [ ! -f index.db -o ! -f files.txt ]
-then
+if [ ! -f index.db -o ! -f files.txt ]; then
     echo "Creating database"
     dbclear index.db
     for i in `cat files2.txt`
@@ -28,19 +29,17 @@ echo "Updating database"
 #Generate the list of files added since last time
 comm -23 files2.txt files.txt > filesadded.txt
 
-for i in `cat filesadded.txt`
-do
+for i in `cat filesadded.txt`; do
     add_to_index $i
 done
 
-for i in `comm -23 files.txt files2.txt` # removed files
-do
+# Remove removed files
+for i in `comm -23 files.txt files2.txt`; do
     remove_from_index $i
 done
 
 # modified files (that weren't added)
-for i in `comm -23 <(find . -newer files.txt -name "*.cpp"| sort) filesadded.txt`
-do
+for i in `comm -23 <(find . -newer files.txt -name "*.cpp" | sort) filesadded.txt`; do
     remove_from_index $i
     add_to_index $i
 done
