2a1707ff432a70b3dced4465738abe75bec5ac39
Kevin Wu Won <exclipy@gmail.com>
2011-10-09 12:02:46 +1100

    Split out a ClicDb.cpp source file.

---
+++ ClicDb.cpp
@@ -0,0 +1,71 @@
+#include "ClicDb.hpp"
+
+#include "clic_printer.hpp"
+#include "clic_parser.hpp"
+#include <boost/foreach.hpp>
+#include <boost/range/algorithm.hpp>
+#include <boost/algorithm/string.hpp>
+#include <sstream>
+#include <iterator>
+
+ClicDb::ClicDb(const char* dbFilename) : db(NULL, 0)
+{
+    try {
+        db.set_error_stream(&std::cerr);
+        db.open(NULL, dbFilename, NULL, DB_BTREE, DB_CREATE, 0);
+    } catch(DbException &e) {
+        std::cerr << "Exception thrown: " << e.what() << std::endl;
+        exit(1);
+    }
+}
+
+ClicDb::~ClicDb() {
+    db.compact(0, 0, 0, 0, DB_FREE_SPACE, 0);
+    db.close(0);
+}
+
+void ClicDb::clear() {
+    try {
+        db.truncate(0, 0, 0);
+    } catch(DbException &e) {
+        std::cerr << "Exception thrown: " << e.what() << std::endl;
+        exit(1);
+    }
+}
+
+void ClicDb::set(const std::string& usr, const std::set<std::string>& locations) {
+    std::stringstream ss;
+    printLocations(ss, locations);
+    std::string locationsString = ss.str();
+    Dbt key(const_cast<char*>(usr.c_str()), usr.size());
+    Dbt value(const_cast<char*>(locationsString.c_str()), locationsString.size());
+    db.put(NULL, &key, &value, 0);
+}
+
+std::set<std::string> ClicDb::get(const std::string& usr) {
+    Dbt key(const_cast<char*>(usr.c_str()), usr.size());
+    Dbt value;
+    if (db.get(NULL, &key, &value, 0) == DB_NOTFOUND)
+        return std::set<std::string>();
+    std::string str((char*)value.get_data(), value.get_size());
+    std::set<std::string> result;
+    boost::algorithm::split(result, str, boost::algorithm::is_any_of("\t"));
+    return result;
+}
+
+void ClicDb::addMultiple(const std::string& usr, const std::set<std::string>& locationsToAdd) {
+    std::set<std::string> storedLocations = get(usr);
+    int originalCount = storedLocations.size();
+    boost::copy(locationsToAdd, std::inserter(storedLocations, storedLocations.begin()));
+    if (storedLocations.size() > originalCount)
+        set(usr, storedLocations);
+}
+
+void ClicDb::rmMultiple(const std::string& usr, const std::set<std::string>& locationsToRemove) {
+    std::set<std::string> storedLocations = get(usr);
+    int originalCount = storedLocations.size();
+    BOOST_FOREACH(std::string loc, locationsToRemove)
+        storedLocations.erase(loc);
+    if (storedLocations.size() < originalCount)
+        set(usr, storedLocations);
+}
--- ClicDb.hpp
+++ ClicDb.hpp
@@ -1,79 +1,23 @@
 #pragma once
 
-#include "clic_printer.hpp"
-#include "clic_parser.hpp"
 #include <db_cxx.h>
-#include <boost/foreach.hpp>
-#include <boost/range/algorithm.hpp>
-#include <boost/algorithm/string.hpp>
 #include <set>
-#include <sstream>
-
-#include <iterator>
 
 class ClicDb {
     public:
-        ClicDb(const char* dbFilename) : db(NULL, 0)
-        {
-            try {
-                db.set_error_stream(&std::cerr);
-                db.open(NULL, dbFilename, NULL, DB_BTREE, DB_CREATE, 0);
-            } catch(DbException &e) {
-                std::cerr << "Exception thrown: " << e.what() << std::endl;
-                exit(1);
-            }
-        }
+        ClicDb(const char* dbFilename);
 
-        void clear() {
-            try {
-                db.truncate(0, 0, 0);
-            } catch(DbException &e) {
-                std::cerr << "Exception thrown: " << e.what() << std::endl;
-                exit(1);
-            }
-        }
+        ~ClicDb();
 
-        void set(const std::string& usr, const std::set<std::string>& locations) {
-            std::stringstream ss;
-            printLocations(ss, locations);
-            std::string locationsString = ss.str();
-            Dbt key(const_cast<char*>(usr.c_str()), usr.size());
-            Dbt value(const_cast<char*>(locationsString.c_str()), locationsString.size());
-            db.put(NULL, &key, &value, 0);
-        }
+        void clear();
 
-        std::set<std::string> get(const std::string& usr) {
-            Dbt key(const_cast<char*>(usr.c_str()), usr.size());
-            Dbt value;
-            if (db.get(NULL, &key, &value, 0) == DB_NOTFOUND)
-                return std::set<std::string>();
-            std::string str((char*)value.get_data(), value.get_size());
-            std::set<std::string> result;
-            boost::algorithm::split(result, str, boost::algorithm::is_any_of("\t"));
-            return result;
-        }
+        void set(const std::string& usr, const std::set<std::string>& locations);
 
-        void addMultiple(const std::string& usr, const std::set<std::string>& locationsToAdd) {
-            std::set<std::string> storedLocations = get(usr);
-            int originalCount = storedLocations.size();
-            boost::copy(locationsToAdd, std::inserter(storedLocations, storedLocations.begin()));
-            if (storedLocations.size() > originalCount)
-                set(usr, storedLocations);
-        }
+        std::set<std::string> get(const std::string& usr);
 
-        void rmMultiple(const std::string& usr, const std::set<std::string>& locationsToRemove) {
-            std::set<std::string> storedLocations = get(usr);
-            int originalCount = storedLocations.size();
-            BOOST_FOREACH(std::string loc, locationsToRemove)
-                storedLocations.erase(loc);
-            if (storedLocations.size() < originalCount)
-                set(usr, storedLocations);
-        }
+        void addMultiple(const std::string& usr, const std::set<std::string>& locationsToAdd);
 
-        ~ClicDb() {
-            db.compact(0, 0, 0, 0, DB_FREE_SPACE, 0);
-            db.close(0);
-        }
+        void rmMultiple(const std::string& usr, const std::set<std::string>& locationsToRemove);
 
     private:
         class ClicCursor {
